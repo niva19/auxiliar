d2aa79f638eb855d1145d38751f9dcb8f038fccf
LuisCarrilloR <30560078+LuisCarrilloR@users.noreply.github.com>
2017-10-02 17:35:17 -0600

    ignores

--- Script Base/BaseDeDatos.sql
+++ Script Base/BaseDeDatos.sql
@@ -59,6 +59,7 @@ ubicacion text,
 codProyecto text,
 constraint pkArchivo primary key (codArchivo),
 constraint fkProyecto foreign key (codProyecto) references Proyecto
+hfhd
 );
 
 /* Tabla de Cliente */
