a7207cfad0b4316c78335a140987ba8fb00e6066
LuisCarrilloR <30560078+LuisCarrilloR@users.noreply.github.com>
2017-10-02 17:40:13 -0600

    sql no se sube siempre

--- Script Base/BaseDeDatos.sql
+++ Script Base/BaseDeDatos.sql
@@ -59,6 +59,7 @@ ubicacion text,
 codProyecto text,
 constraint pkArchivo primary key (codArchivo),
 constraint fkProyecto foreign key (codProyecto) references Proyecto
+dsfd
 );
 
 /* Tabla de Cliente */
