e4c688b7365d68f1cac3aece388631f7ee7e02c1
niva <pknivasabe@gmail.com>
2017-10-01 23:03:28 -0600

    prueba2

--- queries.js
+++ queries.js
@@ -6,7 +6,7 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://postgres:mio@localhost:8485/PROARINSADB';
+var connectionString = 'postgres://postgres:mio@localhost:3030/PROARINSADB';
 var db = pgp(connectionString);
 
 // METER CADA QUERIE DE CADA TABLA EN UNA .JS POR SEPARA !!!!!!!!!!!!!!!!!!!!!!!!!
