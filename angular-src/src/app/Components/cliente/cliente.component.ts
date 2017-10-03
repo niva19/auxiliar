cf67619d398b5b624cd8b66f842e8b61776b78ed
niva <pknivasabe@gmail.com>
2017-10-02 19:12:38 -0600

    eliminar en proceso

--- angular-src/src/app/Components/cliente/cliente.component.ts
+++ angular-src/src/app/Components/cliente/cliente.component.ts
@@ -15,8 +15,12 @@ export class ClienteComponent implements OnInit {
   cedula: String
   telefono: String
   correo: String
-  switch: Boolean = true;
+  switch: Boolean = true
+  borrar: String//variable auxiliar utilizada para guardar la cedula cuando se proceda a borrar
   ax: any[];
+
+  @ViewChild('modal2Footer')
+  private modal2Footer: ElementRef
 
   @ViewChild('LabelNombre')
   private LabelNombre: ElementRef
@@ -55,8 +59,27 @@ export class ClienteComponent implements OnInit {
     alert(v)
   }
 
+  LimpiarGuardar(){
+    this.renderer2.removeClass(this.LabelNombre.nativeElement,"active")
+    this.nombre = ""
+
+    this.renderer2.removeClass(this.LabeApellidos.nativeElement,"active")
+    this.apellidos = ""
+
+    this.renderer2.removeClass(this.LabelCedula.nativeElement,"active")
+    this.renderer2.removeAttribute(this.inputcedula.nativeElement,'disabled');
+    this.cedula = ""
+
+    this.renderer2.removeClass(this.LabelCorreo.nativeElement,"active")
+    this.correo = ""
+
+    this.renderer2.removeClass(this.LabelTelefono.nativeElement,"active")
+    this.telefono = ""
+  }
+
   modal1() {
     this.switch = true;
+    this.LimpiarGuardar();
     $('#modal1').modal('open');
   }
 
@@ -83,13 +106,29 @@ export class ClienteComponent implements OnInit {
 
       this.switch = false
       $('#modal1').modal('open');
-    });
-    
-    
+    }); 
   }
 
   Eliminar(id){
+    let button = this.renderer2.createElement('a');
+    this.renderer2.removeChild(this.modal2Footer.nativeElement,this.modal2Footer.nativeElement.children[1]);
+    // this.modal2Footer.nativeElement.innerHTML ='';
+
+    this.renderer2.setAttribute(button,"class","modal-action")
+    this.renderer2.setAttribute(button,"class","modal-close")
+    this.renderer2.setAttribute(button,"class","waves-effect")
+    this.renderer2.setAttribute(button,"class","waves-green")
+    this.renderer2.setAttribute(button,"class","btn-flat")
+    let txt = this.renderer2.createText("Confirmar")
+    this.renderer2.appendChild(button,txt)
+    this.renderer2.listen(button,'click',()=>{
+      alert(id)
+    })
+
+    this.renderer2.appendChild(this.modal2Footer.nativeElement,button);
+    
     console.log(id)
+    $('#modal2').modal('open');
   }
   ClienteSubmit() {
 
@@ -106,6 +145,7 @@ export class ClienteComponent implements OnInit {
         if (data.success) {
           alert("correcto");
           this.getAll();
+          $('#modal1').modal('close');
         }
         else {
           alert("incorrecto");
@@ -118,13 +158,6 @@ export class ClienteComponent implements OnInit {
         this.getAll();
         this.switch = true;
         $('#modal1').modal('close');
-        // if (data.success) {
-        //   alert("correcto");
-        //   this.getAll();
-        // }
-        // else {
-        //   alert("incorrecto");
-        // }
       });
     }
   }
