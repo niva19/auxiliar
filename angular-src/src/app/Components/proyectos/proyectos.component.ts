0be77801e31d87e0ad722586e624e8867957be0a
AndresCascante <ancas896@gmail.com>
2018-04-09 21:38:14 -0600

    Interfaz Lista

--- angular-src/src/app/Components/proyectos/proyectos.component.ts
+++ angular-src/src/app/Components/proyectos/proyectos.component.ts
@@ -261,8 +261,6 @@ export class ProyectosComponent implements OnInit {
       return false
     if ($('#fechaInicio').val() == '')
       return false
-    if ($('#fechaFinaliza').val() == '')
-      return false
     if (this.estado == '')
       return false
     if (this.inputbanco.nativeElement.value == '')
